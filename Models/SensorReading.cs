ecc54339556bae75df21d37c7eacc10358dbead3
Kabo <30011287@tafe.wa.edu.au>
2026-05-28 14:19:04 +0800

    Add project files.

---
+++ Models/SensorReading.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sensing4U.Models
+{
+    public class SensorReading
+    {
+        public DateTime Timestamp { get; set; } // When the reading was taken
+        public string Label { get; set; } // The type of reading, e.g., "Temperature", "Humidity", "LightLevel"
+        public double? Value { get; set; } // numeric value, nullable in case of non-numeric readings
+        public string Unit { get; set; } // e.g., "°C", "%", "Lux"
+    }
+}
---
+++ Services/FileManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sensing4U.Models;
+
+namespace Sensing4U.Services
+{
+    public static class FileManager
+    {
+
+    }
+}
